c294c80c82d69257584ac56673d3f1ea5e5a74f4
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-02 13:59:37 +0200

    add utility script

---
+++ tira-test-runner-is-trustworthy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+URLS=( google.com github.com gitlab.com webis.de )
+
+which curl  2> /dev/null 1> /dev/null
+
+if [ "$?" != "0" ]; then
+    echo -e " [\033[0;31mx\033[0m] Required tool is not installed. I abort the run"
+    exit 1
+fi
+
+for URL in "${URLS[@]}"
+do
+    CURL_OUTPUT=$(curl "${URL}" 2>&1|grep "http")
+   
+    if [ "$?" == "0" ]; then
+        echo -e " [\033[0;31mx\033[0m] The runner has access to the internet. I abort the run."
+        exit 1
+    fi
+done
+
+echo -e " [\033[0;32mo\033[0m] The runner has no access to the internet."
+
+echo -e "TODO: check only allowed files are readable"
+

f5ffdc5f3aa1faa0cc8464cffb38fd5c6a87c27b
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-02 23:04:46 +0200

    mf

--- tira-specify-task-to-run.sh
+++ tira-specify-task-to-run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "INPUT_RUN=dummy-input-run"
-echo "OUTPUT_DIR=dummy-output-dir"
+echo "OUTPUT_DIR=/tmp/tira-output-dir-tmp-07-22-2022/"
 echo "IMAGE_TO_EXECUTE=ubuntu:16.04"
+echo "COMMAND_TO_EXECUTE=bash -c 'ls /mnt/ceph/tira/data/; ls /mnt/ceph/tira/data/runs/; ls /mnt/ceph/tira/data/datasets/test-datasets-truth/; echo \"${INPUT_RUN}\"; echo \"${OUTPUT_DIR}\"; touch dummy-result-01.jsonl; touch ${OUTPUT_DIR}/dummy-result-02.jsonl'"
 

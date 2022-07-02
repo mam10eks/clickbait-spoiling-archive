037139c315a20c53d9c111645755aea3af5a9ed7
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-02 23:33:40 +0200

    mf

--- tira-specify-task-to-run.sh
+++ tira-specify-task-to-run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "INPUT_RUN=dummy-input-run"
-echo "OUTPUT_DIR=/tmp/tira-output-dir-tmp-07-22-2022/"
-echo "IMAGE_TO_EXECUTE=ubuntu:16.04"
-echo "COMMAND_TO_EXECUTE=bash -c 'ls /mnt/ceph/tira/data/; ls /mnt/ceph/tira/data/runs/; ls /mnt/ceph/tira/data/datasets/test-datasets-truth/; echo \"\${INPUT_RUN}\"; echo \"\${OUTPUT_DIR}\"; touch dummy-result-01.jsonl; touch \${OUTPUT_DIR}/dummy-result-02.jsonl'"
+echo "TIRA_INPUT_RUN=dummy-input-run"
+echo "TIRA_OUTPUT_DIR=/tmp/tira-output-dir-tmp-07-22-2022/"
+echo "TIRA_IMAGE_TO_EXECUTE=ubuntu:16.04"
+echo "TIRA_COMMAND_TO_EXECUTE=bash -c 'ls /mnt/ceph/tira/data/; ls /mnt/ceph/tira/data/runs/; ls /mnt/ceph/tira/data/datasets/test-datasets-truth/; echo \"\${TIRA_INPUT_RUN}\"; echo \"\${TIRA_OUTPUT_DIR}\"; touch dummy-result-01.jsonl; touch \${TIRA_OUTPUT_DIR}/dummy-result-02.jsonl'"
 

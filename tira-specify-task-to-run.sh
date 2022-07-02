99b47b9003f4fdef7d0c84bb237431325dfee8b2
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-03 00:39:02 +0200

    mf

--- tira-specify-task-to-run.sh
+++ tira-specify-task-to-run.sh
@@ -4,7 +4,10 @@ JOB_DIR=$(find -iname job-to-execute.txt|head -1)
 JOB_DIR=$(echo ${JOB_DIR}| awk -F 'job-to-execute' '{print $1}')
 INPUT_DATA=$(echo ${JOB_DIR} |awk -F '/' '{print $2}')
 
-[[ ! -f "${JOB_DIR}/job-to-execute.txt" ]] && exit 0
+if [[ ! -f "${JOB_DIR}/job-to-execute.txt" ]]; then
+    echo "TIRA_IMAGE_TO_EXECUTE=ubuntu:16.04"
+    exit 0
+fi
 
 echo "TIRA_INPUT_RUN=/mnt/ceph/tira/data/datasets/${INPUT_DATA}"
 echo "TIRA_OUTPUT_DIR=${JOB_DIR}/output"

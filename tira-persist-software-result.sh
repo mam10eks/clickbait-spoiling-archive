d1e7f83371793e578332106252ddc973ab33dc28
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-03 13:09:07 +0200

    mf

--- tira-persist-software-result.sh
+++ tira-persist-software-result.sh
@@ -7,14 +7,14 @@ TIRA_EVALUATION_OUTPUT_DIR="$(echo ${SRC_DIR}| awk -F '/output' '{print $1}')/..
 echo "TIRA_EVALUATION_INPUT_DIR=${TARGET_DIR}" >> ${JOB_DIR}/job-to-execute.txt
 echo "TIRA_EVALUATION_OUTPUT_DIR=${TIRA_EVALUATION_OUTPUT_DIR}" >> ${JOB_DIR}/job-to-execute.txt
 
-echo "TIRA_EVALUATION_INPUT_DIR=${TARGET_DIR}" >> task.env
+echo "TIRA_EVALUATION_INPUT_DIR=${TARGET_DIR}" > task.env
 echo "TIRA_EVALUATION_OUTPUT_DIR=${TIRA_EVALUATION_OUTPUT_DIR}" >> task.env
 
 echo "TIRA_EVALUATION_IMAGE_TO_EXECUTE=ubuntu:18.04" >> ${JOB_DIR}/job-to-execute.txt
 echo "TIRA_EVALUATION_COMMAND_TO_EXECUTE=echo '{\"key\":\"value\"}' >> \${TIRA_ECALUATION_OUTPUT_DIR}/evaluation-results.json" >> ${JOB_DIR}/job-to-execute.txt
 
 echo "TIRA_EVALUATION_IMAGE_TO_EXECUTE=ubuntu:18.04" >> task.env
-echo "TIRA_EVALUATION_COMMAND_TO_EXECUTE=echo '{\"key\":\"value\"}' >> \${TIRA_ECALUATION_OUTPUT_DIR}/evaluation-results.json" >> task.env
+echo "TIRA_EVALUATION_COMMAND_TO_EXECUTE=echo '{\"key\":\"value\"}' >> \${TIRA_EVALUATION_OUTPUT_DIR}/evaluation-results.json" >> task.env
 
 if [ -f "$TARGET_DIR" ]; then
     echo "$TARGET_DIR exists already. Exit."

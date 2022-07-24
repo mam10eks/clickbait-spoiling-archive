c8e8d429755b9e7d097f4e06ec4be4d96d2f3802
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-07-24 12:24:03 +0200

    mf

--- src/tira-git-cmd.sh
+++ src/tira-git-cmd.sh
@@ -4,11 +4,15 @@ docker run \
 	-v ${PWD}:/repo \
 	-v /mnt/ceph/tira/:/mnt/ceph/tira/:ro \
 	-v ${HOME}/.ssh/id_rsa:${HOME}/.ssh/id_rsa:ro \
+	-v ${HOME}/.ssh/config:${HOME}/.ssh/config:ro \
 	-v ${HOME}/.ssh/id_rsa.pub:${HOME}/.ssh/id_rsa.pub:ro \
 	-v ${HOME}/.gitconfig:${HOME}/.gitconfig:ro \
+	-v ${SSH_AUTH_SOCK}:${SSH_AUTH_SOCK} \
+	-e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
 	-v /etc/passwd:/etc/passwd:ro \
 	-w /repo \
 	-u $(id -u ${USER}):$(id -g ${USER}) \
+	--entrypoint ./src/tira_git.py \
 	--rm -ti webis/tira-git:0.0.11 ${@}
 
 
